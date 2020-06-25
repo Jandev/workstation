649c73e76951ac16f61e5568091ddffa199f5cfa
Jan de Vries <jan@jan-v.nl>
2020-06-25 22:35:23 +0200

    Added first Farmer application with output

---
+++ src/Program.fs
@@ -0,0 +1,19 @@
+﻿open Farmer
+open Farmer.Builders
+
+let developmentEnvironment = vm {
+    name "dev-jan"
+    username "jan"
+    vm_size Vm.Standard_F8s_v2
+    operating_system Vm.WindowsServer_2019Datacenter
+    os_disk 128 Vm.StandardSSD_LRS
+    add_ssd_disk 512
+}
+
+let deployment = arm {
+    location Location.WestEurope
+    add_resource developmentEnvironment
+}
+
+deployment 
+|> Writer.quickWrite "..\\deployment\\development-environment"
